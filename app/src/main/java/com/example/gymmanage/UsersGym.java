a1a7a7c9e8d516de1378a2a3c3ef51d0c45901fd
mohitshahi <mohitshahi238@gmail.com>
2019-10-13 22:34:07 +0530

    usersgym

---
+++ app/src/main/java/com/example/gymmanage/UsersGym.java
@@ -0,0 +1,15 @@
+package com.example.gymmanage;
+
+public class UsersGym {
+    private UsersGym(){}
+    public static class GymEntry{
+        private static final String TABLE_NAME_CLIENTS="client_details";
+        private static final String NAME="name";
+        private static final String CLIENT_ID="client_id";
+        private static final String PHONENUMBER="phone_number";
+        private static final String SEX="sex";
+        private static final String JOINING_DATE="joining_date";
+        private static final String ENDING_DATE="ending_date";
+        private static final String AMOUNT="amount";
+    }
+}
