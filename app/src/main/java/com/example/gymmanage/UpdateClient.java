c62888b3d569aff49172499eab845d29ca69b472
mohitshahi <mohitshahi238@gmail.com>
2019-10-14 00:22:10 +0530

    usersgym

--- app/src/main/java/com/example/gymmanage/UpdateClient.java
+++ app/src/main/java/com/example/gymmanage/UpdateClient.java
@@ -1,6 +1,7 @@
 package com.example.gymmanage;
 
 
+import android.database.Cursor;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,24 +9,63 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class UpdateClient extends Fragment {
+    ClientDatabase db;
+private Button bnsearch_updatefrag,bnupdate_updatefrag,bndelete_updatefrag;
+private EditText name_tx,phonenumber_tx,gender_tx,enddate_tx,joindate_tx,amount_tx;
 
-
-    public UpdateClient() {
-        // Required empty public constructor
-    }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_update_client, container, false);
+        View view= inflater.inflate(R.layout.fragment_update_client, container, false);
+        db=new ClientDatabase(getActivity());
+        name_tx=view.findViewById(R.id.name_updateclient);
+        bnsearch_updatefrag=view.findViewById(R.id.bnsearch_updateclient);
+        bnsearch_updatefrag=view.findViewById(R.id.bnsearch_updateclient);
+        bnsearch_updatefrag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+searchClient();
+            }
+        });
+
+    return view;
     }
+
+
+    public void searchClient(){
+        String Name=name_tx.getText().toString();
+        String Phonenumber=phonenumber_tx.getText().toString();
+        String Gender=gender_tx.getText().toString();
+        String Joindate=joindate_tx.getText().toString();
+        String Enddate=enddate_tx.getText().toString();
+      String Amount=amount_tx.getText().toString();
+
+
+Cursor cs=db.SearchClient(Name,Phonenumber);
+
+    name_tx.setText(cs.getString(1));
+    phonenumber_tx.setText(cs.getString(2));
+    gender_tx.setText(cs.getString(3));
+    joindate_tx.setText(cs.getString(4));
+    enddate_tx.setText(cs.getString(5));
+    amount_tx.setText(cs.getString(6));
+
+
+
+
+
+    }
+
 
 }
